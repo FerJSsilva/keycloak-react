31a1baefd079176cb4eb6930df908726ee73d0b6
Fernando Silva <development.ferjssilva@gmail.com>
2018-12-03 04:07:42 -0200

    feature: add simple keycloak authentication

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import Keycloak from 'keycloak-js';
 import App from './App';
 
-ReactDOM.render(
-  React.createElement(App),
-  document.getElementById('root'),
-);
+const keycloak = Keycloak({
+  url: 'http://localhost:8080/auth',
+  realm: 'myrealm',
+  clientId: 'myApp',
+});
+
+keycloak.init({ onLoad: 'login-required' }).success((authenticated) => {
+  if (authenticated) {
+    ReactDOM.render(
+      React.createElement(App),
+      document.getElementById('root'),
+    );
+  }
+});
 
 // Check if hot reloading is enable. If it is, changes won't reload the page.
 // This is related to webpack-dev-server and works on development only.
