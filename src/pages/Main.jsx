6b12e8c603711d7e19de7c384de2b6018f0a6a47
Fernando Silva <development.ferjssilva@gmail.com>
2018-12-03 03:03:57 -0200

    feature: remove sidebar from main page

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -10,7 +10,6 @@ import { Layout } from 'antd';
 
 import Header from '../layout/headerComponent/HeaderComponent';
 import Footer from '../layout/footerComponent/FooterComponent';
-import Sidebar from '../layout/sidebarComponent/SidebarComponent';
 
 import Home from './home/Home';
 import Sample from './sample/Sample';
@@ -23,7 +22,6 @@ const { Content } = Layout;
 const Main = () => (
   <Router>
     <Layout style={{ minHeight: '100vh' }}>
-      <Sidebar />
       <Layout>
         <Header />
         <Content style={{ margin: '16px 16px 0 16px' }}>
