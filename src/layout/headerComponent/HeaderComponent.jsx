0d7c0f30f48154b0539bbe8b205c5092fd4d9c68
Fernando Silva <development.ferjssilva@gmail.com>
2018-12-03 04:29:34 -0200

    feature: use keycloak token from redux to logout and show username

--- src/layout/headerComponent/HeaderComponent.jsx
+++ src/layout/headerComponent/HeaderComponent.jsx
@@ -1,42 +1,61 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import { Col, Dropdown, Icon, Layout, Menu, Row } from 'antd';
 
 const { Header } = Layout;
 
-const menu = (
-  <Menu>
-    <Menu.Item key="0">
-      <a target="blank" href="https://github.com/FerJSsilva/Odds-Reactor">1st menu item</a>
-    </Menu.Item>
-    <Menu.Item key="1">
-      <a target="blank" href="https://github.com/FerJSsilva/Odds-Reactor">2nd menu item</a>
-    </Menu.Item>
-    <Menu.Divider />
-    <Menu.Item key="3">3rd menu item</Menu.Item>
-  </Menu>
-);
+class HeaderComponent extends Component {
+  static propTypes = {
+    keycloak: PropTypes.objectOf(PropTypes.any),
+  };
 
-const HeaderComponent = () => (
-  <Header
-    className="custom-header"
-  >
-    <Row>
-      <Col span={12} offset={6} style={{ display: 'flex', justifyContent: 'center' }}>
-        <div>
-          <h1>React Keycloak App</h1>
-        </div>
-      </Col>
-      <Col span={6} style={{ display: 'flex', justifyContent: 'flex-end' }}>
-        <div className="user-info">
-          <Dropdown overlay={menu} trigger={['click']}>
-            <span style={{ marginLeft: '16px' }}>
-              user name <Icon type="down" />
-            </span>
-          </Dropdown>
-        </div>
-      </Col>
-    </Row>
-  </Header>
-);
+  static defaultProps = {
+    keycloak: {},
+  };
 
-export default HeaderComponent;
+  logoutUser = () => {
+    if (this.props.keycloak) {
+      this.props.keycloak.logout();
+    }
+  }
+
+  menu = (
+    <Menu>
+      <Menu.Item key="0">
+        <button onClick={this.logoutUser}>logout</button>
+      </Menu.Item>
+    </Menu>
+  );
+
+  render() {
+    return (
+      <Header
+        className="custom-header"
+      >
+        <Row>
+          <Col span={12} offset={6} style={{ display: 'flex', justifyContent: 'center' }}>
+            <div>
+              <h1>React Keycloak App</h1>
+            </div>
+          </Col>
+          <Col span={6} style={{ display: 'flex', justifyContent: 'flex-end' }}>
+            <div className="user-info">
+              <Dropdown overlay={this.menu} trigger={['click']}>
+                <span style={{ marginLeft: '16px' }}>
+                  Welcome {this.props.keycloak && this.props.keycloak.tokenParsed.name}<Icon type="down" />
+                </span>
+              </Dropdown>
+            </div>
+          </Col>
+        </Row>
+      </Header>
+    );
+  }
+}
+
+const mapStateToProps = ({ keycloak }) => ({
+  keycloak,
+});
+
+export default connect(mapStateToProps)(HeaderComponent);
